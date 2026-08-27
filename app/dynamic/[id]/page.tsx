ae43561a728b1e3d6d34ec0e61107b7dfffce5c1
Foam <ddotdka4@gmail.com>
2026-08-27 23:58:36 +0700

    feat: add dynamic id route page and update tsconfig jsx setting to preserve

---
+++ app/dynamic/[id]/page.tsx
@@ -0,0 +1,13 @@
+const DynamicPage = async ({ params }: { params: Promise<{ id: string }> }) => {
+   
+    const { id } = await params
+
+
+
+    return (
+        <div>
+            <h1>{id} </h1>
+        </div>
+    )
+}
+export default DynamicPage
